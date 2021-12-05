4b86f8bcc5d3c81246137051611c8c39fc33826c
Augustine Kwabena Antwi <antwigoodness@gmail.com>
2021-12-05 18:56:39 +0000

    update

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import OnlineShoppingScreen from './components/OnlineShoppingScreen';
+import AddToCartScreen from './components/AddToCartScreen';
+import PaymentSuccessfulScreen from './components/PaymentSuccessfulScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <OnlineShoppingScreen/>
     </View>
   );
 }
@@ -14,8 +16,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:70
   },
 });
---
+++ components/PaymentSuccessfulScreen.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { View ,Text,Image, TouchableOpacity,StyleSheet} from 'react-native'
+
+export default class PaymentSuccessfulScreen extends Component {
+    render() {
+        return (
+            <View style ={styles.container}>
+                <View style ={styles.headerContainer}>
+                    <Text style={styles.headerText}>PAYMENT SUCCESSFUL</Text>
+                </View>
+                <View style= {styles.info}>
+                    <Text style={styles.infoText} numberOfLines={5}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                     quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                    </Text>
+                </View>
+                <Image style={styles.Image} source={require('../assets/payment-successful.png')} />
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Next</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        marginHorizontal:20
+
+    },
+    headerContainer:{
+        marginVertical:10,
+    },
+    headerText:{
+        fontSize:30,
+        fontWeight:'bold',
+    },
+    infoText:{
+        color:"#aaaaaa",
+        lineHeight:30,
+        fontSize:16
+    },
+    Image:{
+        height:300,
+        width:300,
+        marginVertical:10,
+    },
+    button:{
+        justifyContent:"center",
+        alignItems:"center",
+        height:60,
+        backgroundColor:"#5100ad",
+        borderRadius:50,
+        marginHorizontal:80
+
+    },
+    buttonText:{
+        color:'white',
+        fontWeight:"bold",
+        fontSize:20,
+    }
+
+})
